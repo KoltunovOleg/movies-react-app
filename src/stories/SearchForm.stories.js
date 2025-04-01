42b72efeea52c26b9074a7e6b2c339a0a94bd7e7
Oleh Koltunov <oleh_koltunov@epam.com>
2025-04-01 22:08:50 +0200

    Module_3 | Write Storybook stories for components

---
+++ src/stories/SearchForm.stories.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import SearchForm from '../components/SearchForm/SearchForm';
+
+export default {
+  title: 'Components/SearchForm',
+  component: SearchForm,
+  argTypes: {
+    initialQuery: {
+      control: { type: 'text' },
+      defaultValue: '',
+      description: 'The initial value of the search input',
+    },
+    onSearch: { action: 'onSearch' },
+  },
+};
+
+const Template = (args) => <SearchForm {...args} />;
+
+// Default story
+export const Default = Template.bind({});
+Default.args = {
+  initialQuery: '',
+};
+
+// Story with a prefilled query
+export const PrefilledQuery = Template.bind({});
+PrefilledQuery.args = {
+  initialQuery: 'React',
+};
+
+// Story with a long query
+export const LongQuery = Template.bind({});
+LongQuery.args = {
+  initialQuery: 'A long search query that demonstrates how the input handles larger text',
+};
